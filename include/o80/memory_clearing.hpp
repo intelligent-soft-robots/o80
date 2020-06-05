56223d356dbeb441d94a282704776781197f65d2
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-06-05 09:34:54 +0200

    memory clearing now clearing everything

--- include/o80/memory_clearing.hpp
+++ include/o80/memory_clearing.hpp
@@ -1,26 +1,24 @@
 #pragma once
 
-#include "shared_memory/mutex.hpp"
 #include "shared_memory/shared_memory.hpp"
+#include "time_series/multiprocess_time_series.hpp"
+#include "o80/burster.hpp"
 
 namespace o80
 {
 void clear_shared_memory(std::string segment_id)
 {
+
+  time_series::clear_memory(segment_id + "_commands");
+  time_series::clear_memory(segment_id + "_observations");
+  time_series::clear_memory(segment_id + "_completed");
+  Burster::clear_memory(segment_id);
+  shared_memory::clear_shared_memory(segment_id +
+				     std::string("_synchronizer"));
+  shared_memory::clear_shared_memory(segment_id +
+				     std::string("_synchronizer_follower"));
+  shared_memory::clear_shared_memory(segment_id +
+				     std::string("_synchronizer_leader"));
     shared_memory::clear_shared_memory(segment_id);
-    shared_memory::clear_shared_memory(segment_id + std::string("_commands"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_observations"));
-    shared_memory::clear_shared_memory(segment_id + std::string("_completed"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer_follower"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer_leader"));
-    // mutex cleaned on destruction
-    shared_memory::Mutex m1(segment_id + std::string("_locker"), true);
-    shared_memory::Mutex m2(
-        std::string("completed_") + segment_id + std::string("_locker"), true);
 }
 }
