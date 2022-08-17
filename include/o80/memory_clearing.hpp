384288477c05d5a15a479a7305fa2fa073224f3d
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-08-17 16:07:48 +0200

    added memory_clearing.cpp

--- include/o80/memory_clearing.hpp
+++ include/o80/memory_clearing.hpp
@@ -13,32 +13,5 @@ namespace o80
  *  been called). Starting a BackEnd using a segment id that
  *  has not been properly cleared will result in the program hanging.
  */
-void clear_shared_memory(std::string segment_id)
-{
-    time_series::clear_memory(segment_id + "_commands");
-    time_series::clear_memory(segment_id + "_observations");
-    time_series::clear_memory(segment_id + "_completed");
-    time_series::clear_memory(segment_id + "_waiting_for_completion");
-    time_series::clear_memory(segment_id + "_completion_reported");
-    time_series::clear_memory(segment_id + "_received");
-    time_series::clear_memory(segment_id + "_starting");
-    shared_memory::clear_shared_memory(segment_id + std::string("_commands"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_observations"));
-    shared_memory::clear_shared_memory(segment_id + std::string("_completed"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_waiting_for_completion"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_completion_reported"));
-    shared_memory::clear_shared_memory(segment_id + std::string("_received"));
-    shared_memory::clear_shared_memory(segment_id + std::string("_starting"));
-    Burster::clear_memory(segment_id);
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer_follower"));
-    shared_memory::clear_shared_memory(segment_id +
-                                       std::string("_synchronizer_leader"));
-    shared_memory::clear_shared_memory(segment_id);
-}
+  void clear_shared_memory(std::string segment_id);
 }  // namespace o80
